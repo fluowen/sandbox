2e929350804a3a17b11f2d4d9cba6e6618942008
Fluowen <contact@fluowen.com>
2024-10-12 12:25:51 -0300

    Commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-// Custom JavaScript can go here
+// Custom JavaScript
+
+// Inicializa o carrossel com intervalo de 3 segundos
+document.addEventListener('DOMContentLoaded', function() {
+    const carouselElement = document.querySelector('#carouselExampleCaptions');
+    const carousel = new bootstrap.Carousel(carouselElement, {
+        interval: 3000, // Troca de slide a cada 3 segundos
+        ride: 'carousel'
+    });
+});
